4510e40f87de8c4b48882b5aeb0aad379ce1b3fc
zhouxuanyou <1102587121@qq.com>
2019-03-13 18:29:21 +0800

    p2p金融网站借款申请，分页和投资前后端交互完成

--- modules/gerenzhongxin/gerenzhongxin.js
+++ modules/gerenzhongxin/gerenzhongxin.js
@@ -1,5 +1,6 @@
 $(function () {
     getuserinfo();
+    $('#btn_charge').on('click', clickCharge)
 });
 function getuserinfo() {
     if (sessionStorage.islogin === 'true'){
@@ -26,4 +27,40 @@ function getuserinfo() {
     }
 
 
+}
+
+//点击用户充值
+function clickCharge(){
+    var datas = {
+                money: 1000000
+            };
+    var success = function (res) {
+        if(res === 'ok'){
+            //重新刷新用户数据，显示充值完毕的金额!
+            alert('充值成功!');
+            getuserinfo()
+        }
+    };
+
+
+    apiget('/savemoney.php',success,datas);
+
+    // $.ajax({
+    //     type: 'get',
+    //     url: 'http://localhost:80/savemoney.php',
+    //     xhrFields: {
+    //         withCredentials: true   //是否携带cookie true允许
+    //     },
+    //     data: {
+    //         money: 10000
+    //     },
+    //     crossDomain: true,  //是否跨域
+    //     success: function(res){
+    //         if(res == 'ok'){
+    //             //重新刷新用户数据，显示充值完毕的金额!
+    //             alert('充值成功!')
+    //             getuserinfo()
+    //         }
+    //     }
+    // })
 }
